1bce5ed1b52b4da2dfcf7dc2f29b2bc895adaf14
Mari Rosenwald <marirosenwald@Maris-MBP-2.domain>
2023-06-28 11:21:15 -0700

    ignoring build zip file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,9 @@ function Home() {
     <br></br>
     <Box class="box">
       <h2 class="center" style={{margin: 5, fontSize: 30, }}>Marina Rosenwald</h2>
-      <p style={{margin: 8, fontSize: 20, }}>Hello and thank you so much for visiting my website :) Here is a great place to learn all about me. Whether it is my previous programs, current projects, or my everyday fun activities you'll find almost everything there is about me here! </p>
+      <p style={{margin: 8, fontSize: 20, }}>
+      Welcome to my website, and thank you for visiting. I extend my sincerest appreciation for your time and interest. My name is Marina Rosenwald, and I am a highly motivated professional with a recent Bachelor of Science degree in Computer Science from California Polytechnic State University. Having successfully completed my internship and fulfilled my university commitments, I embarked on a transformative journey of travel for the past couple of months. Now that I have returned, I am eagerly prepared to embrace new and exciting opportunities in the professional realm. 
+      </p>
     </Box>
 
     </div>
