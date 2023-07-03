642a15fb223027d9cd37bc86e2d123f17cc9bd63
Mari Rosenwald <marirosenwald@Maris-MBP-2.domain>
2023-07-02 23:44:19 -0700

    added SLO to bio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
       <h2 class="center" style={{margin: 5, fontSize: 30, }}>Marina Rosenwald</h2>
       <p style={{margin: 8, fontSize: 20, }}>
 
-      Welcome to my website, and thank you for visiting. I extend my sincerest appreciation for your time and interest. My name is Marina Rosenwald, and I am a highly motivated professional with a recent Bachelor of Science degree in Computer Science from California Polytechnic State University. Having successfully completed my internship and fulfilled my university commitments, I embarked on a transformative journey of travel for the past couple of months. Now that I have returned, I am eagerly prepared to embrace new and exciting opportunities in the professional realm. 
+      Welcome to my website, and thank you for visiting. I extend my sincerest appreciation for your time and interest. My name is Marina Rosenwald, and I am a highly motivated professional with a recent Bachelor of Science degree in Computer Science from California Polytechnic State University - San Luis Obispo. Having successfully completed my internship and fulfilled my university commitments, I embarked on a transformative journey of travel for the past couple of months. Now that I have returned, I am eagerly prepared to embrace new and exciting opportunities in the professional realm. 
       
       </p>
     </Box>
