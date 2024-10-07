fbfa2bf53ba8749e51bf3e6ff88bed20d1aa1690
marinarosenwald <marinarosenwald@gmail.com>
2024-10-07 14:41:16 -0700

    added description to projects, reference to INL, and photos to personal blog

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import "./CodeStyle.css"; 
-import "../components/displayBox.css"; 
-import web from "../photos/website-icon.png"; 
-import wordle from "../photos/wordleIcon.png"; 
-import python from "../photos/python.png"; 
-import snowman from "../photos/snowman.png"; 
+import "./CodeStyle.css";
+import "../components/displayBox.css";
+import web from "../photos/website-icon.png";
+import wordle from "../photos/wordleIcon.png";
+import python from "../photos/python.png";
+import snowman from "../photos/snowman.png";
 import ml from "../photos/ML.png";
+import INL from "../photos/INL_Logo.png"
 
 function Box({ children, ...props }) {
   return <div {...props}>{children}</div>
@@ -13,48 +14,62 @@ function Box({ children, ...props }) {
 
 const codeList = [
   {
-    link: 'https://github.com/marinarosenwald/CSS581MachineLearningHomework', 
+    link: 'https://github.com/UWB-Biocomputing',
+    src: INL,
+    alt: "Lab",
+    text: "Intelligent Networks Laboratory GitHub",
+    context: "This is the GitHub page for the Intelligent Networks Laboratory. Here you can see the Simulator \"Graphitti\" and relavant pages assiciated with it's development. \nNote: I upkeep the lab's website which is a private repo on this page and much of my research-related code will not be publicly available until my thesis is published."
+  },
+  {
+    link: 'https://github.com/marinarosenwald/CSS581MachineLearningHomework',
     src: ml,
-    alt: "MLIcon", 
-    text: "4 large scale ML homework assignments", 
-  }, 
+    alt: "MLIcon",
+    text: "4 large scale ML homework assignments",
+    context: "In these projects you will see the application of linear modeling, Naïve Bayes, logistic regression, random forest, AdaBoost, gradient boost, decision trees, and various clustering algorithms. These assignments also include the data cleaning steps and a clear written examples of my thought process throughout the assignments."
+  },
   {
-    link: 'https://github.com/marinarosenwald/personalWebsite', 
-    src: web,
-    alt: "webIcon", 
-    text: "Code for this Website - React, JavaScript, CSS", 
-  }, 
-  {
-    link: 'https://marinarosenwald.github.io/CSS551FinalWebGL/', 
+    link: 'https://marinarosenwald.github.io/CSS551FinalWebGL/',
     src: snowman,
-    alt: "snowmanIcon", 
-    text: "snowman game (main focus: 3D graphics)", 
-  }, 
+    alt: "snowmanIcon",
+    text: "snowman game (main focus: 3D graphics)",
+    context: "This game was developed as a partner final project for Advanced 3D Graphics. The code for this game is available upon request."
+
+  },
   {
-    link: 'https://github.com/marinarosenwald/CSS583_KMS_Project', 
+    link: 'https://github.com/marinarosenwald/CSS583_KMS_Project',
     src: python,
-    alt: "pythonIcon", 
-    text: "Custom dictionary web-application with microservices and LLM integration", 
-  }, 
+    alt: "pythonIcon",
+    text: "Custom dictionary web-application with microservices and LLM integration",
+    context: "This project was created as a group final project for CSS583 - Knowledge management Systems. The goal of this project was to create a website that implemented both a MongoDB database search and a __ LLM to give the user a definition on organization specific language. Find our final paper about this project here: ___"
+  },
   {
-    link: 'https://github.com/marinarosenwald/CSS583_KMS_LLMs', 
+    link: 'https://github.com/marinarosenwald/CSS583_KMS_LLMs',
     src: python,
-    alt: "pythonIcon", 
-    text: "LLM implementation and prompt engineering", 
-  }, 
+    alt: "pythonIcon",
+    text: "LLM implementation and prompt engineering",
+    context: "The goal of this project was to apply, analyze, and compare Large Language Models. We were looking specifically at the models' success when applied to academic topics and text. We looked at both Summarization and Text Generation Models. This project was done in partnership with Nathanial Jewel."
+  },
   {
-    link: 'https://github.com/marinarosenwald/PythonWordle', 
+    link: 'https://github.com/marinarosenwald/personalWebsite',
+    src: web,
+    alt: "webIcon",
+    text: "Code for this Website - React, JavaScript, CSS",
+    context: "This webite was developed using ReactJS. This is a link to the JavaScript protion of the website. Please note that some of the content of this website was left off of GitHub for privacy reasons."
+  },
+  {
+    link: 'https://github.com/marinarosenwald/PythonWordle',
     src: wordle,
-    alt: "wordleIcon", 
-    text: "Recreating Wordle - Python", 
-  }, 
-//   {
-//     link: 'https://github.com/marinarosenwald/interviewPrep', 
-//     src: python,
-//     alt: "pythonIcon", 
-//     text: "Practice interview questions - Python", 
-//   }, 
-  
+    alt: "wordleIcon",
+    text: "Recreating Wordle - Python",
+    context: "This was a fun project that I did in an evening because my mom was disappointed that she could only play one word a day in wordle."
+  },
+  //   {
+  //     link: 'https://github.com/marinarosenwald/interviewPrep', 
+  //     src: python,
+  //     alt: "pythonIcon", 
+  //     text: "Practice interview questions - Python", 
+  //   }, 
+
 
 ];
 
@@ -72,9 +87,9 @@ function Code() {
     <div id='Code'>
       <br></br>
       <div id="ousideBox">
-        <Box class="box" id="disclaimer"> 
-          <h1 style={{color: 'rgb(194, 125, 15)',}}>Code</h1>
-          <p>Please note that some repos may be private. Contact me @marinarosenwald@gmail.com for access.</p> 
+        <Box class="box" id="disclaimer">
+          <h1 style={{ color: 'rgb(194, 125, 15)', }}>Code</h1>
+          <p>Please note that some repos may be private. Contact me @marinarosenwald@gmail.com for access.</p>
         </Box>
       </div>
       <br></br>
@@ -82,18 +97,24 @@ function Code() {
       {rows.map((row, rowIndex) => (
         <div class="row" key={rowIndex}>
           {row.map((code, columnIndex) => (
-            <div class="column" key={columnIndex}>
+            // <div class="column" key={columnIndex}> 
               <Box class="boxList">
-                <a href={code.link}>
-                  <div class='DinsideBox'>
+                <div class='DinsideBox'>
+
+                  <a href={code.link}>
                     <img src={code.src} alt={code.alt} class="Dicon"></img>
-                    <div class="DinsideText">
-                      <p style={{color: 'rgb(194, 125, 15)', }}>{code.text}</p>
-                    </div>
+                  </a>
+                  <div class="DinsideText">
+                    <a href={code.link}>
+                      <p style={{ color: 'rgb(194, 125, 15)', }}>{code.text}</p>
+                    </a>
+                    <br></br>
+                    <p>{code.context}</p>
                   </div>
-                </a>
+
+                </div>
               </Box>
-            </div>
+            // </div>
           ))}
         </div>
       ))}
--- src/pages/PersonalBlog.js
+++ src/pages/PersonalBlog.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import "./PersonalBlog.css"
-import "../components/displayBox.css"; 
+import "../components/displayBox.css";
 
-import grad from "../photos/Travel/grad.jpeg"; 
+import grad from "../photos/Travel/grad.jpeg";
 import grad1 from "../photos/Travel/grad1.jpeg";
-import spain from "../photos/Travel/spain.jpeg"; 
-import spain1 from "../photos/Travel/spain1.jpeg"; 
-import spain2 from "../photos/Travel/spain2.jpeg"; 
-import spain3 from "../photos/Travel/spain3.jpeg"; 
-import spain4 from "../photos/Travel/spain4.jpeg"; 
-import spain5 from "../photos/Travel/spain5.jpeg"; 
-import spain6 from "../photos/Travel/spain6.jpeg"; 
-import spain7 from "../photos/Travel/spain7.jpeg"; 
-import spain8 from "../photos/Travel/spain8.jpeg"; 
-import spain9 from "../photos/Travel/spain9.jpeg"; 
-import spain11 from "../photos/Travel/spain11.jpeg"; 
-import spain12 from "../photos/Travel/spain12.jpeg"; 
-import spain13 from "../photos/Travel/spain13.jpeg"; 
-import spain14 from "../photos/Travel/spain14.jpeg"; 
-import spain15 from "../photos/Travel/spain15.jpeg"; 
+import spain from "../photos/Travel/spain.jpeg";
+import spain1 from "../photos/Travel/spain1.jpeg";
+import spain2 from "../photos/Travel/spain2.jpeg";
+import spain3 from "../photos/Travel/spain3.jpeg";
+import spain4 from "../photos/Travel/spain4.jpeg";
+import spain5 from "../photos/Travel/spain5.jpeg";
+import spain6 from "../photos/Travel/spain6.jpeg";
+import spain7 from "../photos/Travel/spain7.jpeg";
+import spain8 from "../photos/Travel/spain8.jpeg";
+import spain9 from "../photos/Travel/spain9.jpeg";
+import spain11 from "../photos/Travel/spain11.jpeg";
+import spain12 from "../photos/Travel/spain12.jpeg";
+import spain13 from "../photos/Travel/spain13.jpeg";
+import spain14 from "../photos/Travel/spain14.jpeg";
+import spain15 from "../photos/Travel/spain15.jpeg";
 import vat from "../photos/Travel/vat.jpeg";
-import italy from "../photos/Travel/italy.jpeg"; 
-import italy1 from "../photos/Travel/italy1.jpeg"; 
-import italy2 from "../photos/Travel/italy2.jpeg"; 
-import italy3 from "../photos/Travel/italy3.jpeg"; 
-import italy4 from "../photos/Travel/italy4.jpeg"; 
-import italy5 from "../photos/Travel/italy5.jpeg"; 
-import italy6 from "../photos/Travel/italy6.jpeg"; 
-import italy7 from "../photos/Travel/italy7.jpeg"; 
+import italy from "../photos/Travel/italy.jpeg";
+import italy1 from "../photos/Travel/italy1.jpeg";
+import italy2 from "../photos/Travel/italy2.jpeg";
+import italy3 from "../photos/Travel/italy3.jpeg";
+import italy4 from "../photos/Travel/italy4.jpeg";
+import italy5 from "../photos/Travel/italy5.jpeg";
+import italy6 from "../photos/Travel/italy6.jpeg";
+import italy7 from "../photos/Travel/italy7.jpeg";
 import italy8 from "../photos/Travel/italy8.jpeg";
-import menorca from "../photos/Travel/menorca.jpeg"; 
-import menorca1 from "../photos/Travel/menorca1.jpeg"; 
+import menorca from "../photos/Travel/menorca.jpeg";
+import menorca1 from "../photos/Travel/menorca1.jpeg";
 // import menorca2 from "../photos/Travel/menorca2.JPG"; 
-import menorca3 from "../photos/Travel/menorca3.jpeg"; 
-import menorca4 from "../photos/Travel/menorca4.jpeg"; 
-import menorca5 from "../photos/Travel/menorca5.jpeg"; 
-import menorca6 from "../photos/Travel/menorca6.jpeg"; 
+import menorca3 from "../photos/Travel/menorca3.jpeg";
+import menorca4 from "../photos/Travel/menorca4.jpeg";
+import menorca5 from "../photos/Travel/menorca5.jpeg";
+import menorca6 from "../photos/Travel/menorca6.jpeg";
 import sanFran from "../photos/Travel/sanFran.jpeg";
 import santaBarb from "../photos/Travel/santaBarb.jpeg";
 import slo from "../photos/Travel/slo.jpeg";
@@ -54,311 +54,345 @@ import lake2 from "../photos/Travel/lake2.jpeg";
 import si from "../photos/Travel/si.jpeg";
 import stevens from "../photos/Travel/stevens.jpeg";
 import spokane from "../photos/Travel/spokane.jpeg";
+import spokane1 from "../photos/Travel/spokane1.jpeg";
+import spokane2 from "../photos/Travel/spokane2.jpeg";
+import spokane3 from "../photos/Travel/spokane3.jpeg";
 import newport from "../photos/Travel/newport.jpeg";
 import newport1 from "../photos/Travel/newport1.jpeg";
 import redbuff from "../photos/Travel/redbuff.jpeg";
 import bellevue from "../photos/Travel/bellevue.jpeg";
-
-
+import colorado from "../photos/Travel/colorado.jpeg";
+import seattle from "../photos/Travel/seattle.jpeg";
+import portland from "../photos/Travel/portland.jpeg";
 
 const imagesList = [
-    {
-        id: grad,
-        src: grad,
-        alt: "grad",
-      },
-      {
-        id: grad1,
-        src: grad1,
-        alt: "grad1",
-      },
-    {
-      id: spain,
-      src: spain,
-      alt: "spain",
-    },
-    {
-        id: spain1,
-        src: spain1,
-        alt: "spain1",
-      },
-      {
-        id: spain2,
-        src: spain2,
-        alt: "spain2",
-      },
-      {
-        id: spain3,
-        src: spain3,
-        alt: "spain3",
-      },
-      {
-        id: spain4,
-        src: spain4,
-        alt: "spain4",
-      },
-      {
-        id: spain5,
-        src: spain5,
-        alt: "spain5",
-      },
-      {
-        id: spain6,
-        src: spain6,
-        alt: "spain6",
-      },
-      {
-        id: spain7,
-        src: spain7,
-        alt: "spain7",
-      },
-      {
-        id: spain8,
-        src: spain8,
-        alt: "spain8",
-      },
-      {
-        id: spain9,
-        src: spain9,
-        alt: "spain9",
-      },
-      {
-        id: spain11,
-        src: spain11,
-        alt: "spain11",
-      },
-      {
-        id: spain12,
-        src: spain12,
-        alt: "spain12",
-      },
-      {
-        id: spain13,
-        src: spain13,
-        alt: "spain13",
-      },
-      {
-        id: spain14,
-        src: spain14,
-        alt: "spain14",
-      },
-      {
-        id: spain15,
-        src: spain15,
-        alt: "spain15",
-      },
-      {
-        id: vat,
-        src: vat,
-        alt: "vat",
-      },
-      {
-        id: italy,
-        src: italy,
-        alt: "italy",
-      },
-      {
-        id: italy1,
-        src: italy1,
-        alt: "italy1",
-      },
-      {
-        id: italy2,
-        src: italy2,
-        alt: "italy2",
-      },
-      {
-        id: italy3,
-        src: italy3,
-        alt: "italy3",
-      },
-      {
-        id: italy4,
-        src: italy4,
-        alt: "italy4",
-      },
-      {
-        id: italy5,
-        src: italy5,
-        alt: "italy5",
-      },
-      {
-        id: italy6,
-        src: italy6,
-        alt: "italy6",
-      },
-      {
-        id: italy7,
-        src: italy7,
-        alt: "italy7",
-      },
-      {
-        id: italy8,
-        src: italy8,
-        alt: "italy8",
-      },
-      {
-        id: menorca,
-        src: menorca,
-        alt: "menorca",
-      },
-      {
-        id: menorca1,
-        src: menorca1,
-        alt: "menorca1",
-      },
-      {
-        id: menorca3,
-        src: menorca3,
-        alt: "menorca3",
-      },
-      {
-        id: menorca4,
-        src: menorca4,
-        alt: "menorca4",
-      },
-      {
-        id: menorca5,
-        src: menorca5,
-        alt: "menorca5",
-      },
-      {
-        id: menorca6,
-        src: menorca6,
-        alt: "menorca6",
-      },
-      {
-        id: sanFran,
-        src: sanFran,
-        alt: "sanFran",
-      },
-      {
-        id: santaBarb,
-        src: santaBarb,
-        alt: "santaBarb",
-      },
-      {
-        id: slo,
-        src: slo,
-        alt: "slo",
-      },
-      {
-        id: slo1,
-        src: slo1,
-        alt: "slo1",
-      },
-      {
-        id: slo2,
-        src: slo2,
-        alt: "slo2",
-      },
-      {
-        id: slo3,
-        src: slo3,
-        alt: "slo3",
-      },
-      {
-        id: slo4,
-        src: slo4,
-        alt: "slo4",
-      },
-      {
-        id: slo5,
-        src: slo5,
-        alt: "slo5",
-      },
-      {
-        id: la,
-        src: la,
-        alt: "la",
-      },
-      {
-        id: la1,
-        src: la1,
-        alt: "la1",
-      },
-      {
-        id: la2,
-        src: la2,
-        alt: "la2",
-      },
-      {
-        id: la3,
-        src: la3,
-        alt: "la3",
-      },
-      {
-        id: lake,
-        src: lake,
-        alt: "lake",
-      },
-      {
-        id: lake2,
-        src: lake2,
-        alt: "lake2",
-      },
-      {
-        id: si,
-        src: si,
-        alt: "si",
-      },
-      {
-        id: stevens,
-        src: stevens,
-        alt: "stevens",
-      },
-      {
-        id: spokane,
-        src: spokane,
-        alt: "spokane",
-      },
-      {
-        id: newport,
-        src: newport,
-        alt: "newport",
-      },
-      {
-        id: newport1,
-        src: newport1,
-        alt: "newport1",
-      },
-      {
-        id: redbuff,
-        src: redbuff,
-        alt: "redbuff",
-      },
-      {
-        id: bellevue,
-        src: bellevue,
-        alt: "bellevue",
-      },
+  {
+    id: grad,
+    src: grad,
+    alt: "grad",
+  },
+  {
+    id: grad1,
+    src: grad1,
+    alt: "grad1",
+  },
+  {
+    id: colorado,
+    src: colorado,
+    alt: "colorado",
+  },
+  {
+    id: spokane1,
+    src: spokane1,
+    alt: "spokane1",
+  },
+  {
+    id: spokane2,
+    src: spokane2,
+    alt: "spokane2",
+  },
+  {
+    id: spokane3,
+    src: spokane3,
+    alt: "spokane3",
+  },
+  {
+    id: portland,
+    src: portland,
+    alt: "portland",
+  },
+  {
+    id: seattle,
+    src: seattle,
+    alt: "seattle",
+  },
+  {
+    id: spain,
+    src: spain,
+    alt: "spain",
+  },
+  {
+    id: spain1,
+    src: spain1,
+    alt: "spain1",
+  },
+  {
+    id: spain2,
+    src: spain2,
+    alt: "spain2",
+  },
+  {
+    id: spain3,
+    src: spain3,
+    alt: "spain3",
+  },
+  {
+    id: spain4,
+    src: spain4,
+    alt: "spain4",
+  },
+  {
+    id: spain5,
+    src: spain5,
+    alt: "spain5",
+  },
+  {
+    id: spain6,
+    src: spain6,
+    alt: "spain6",
+  },
+  {
+    id: spain7,
+    src: spain7,
+    alt: "spain7",
+  },
+  {
+    id: spain8,
+    src: spain8,
+    alt: "spain8",
+  },
+  {
+    id: spain9,
+    src: spain9,
+    alt: "spain9",
+  },
+  {
+    id: spain11,
+    src: spain11,
+    alt: "spain11",
+  },
+  {
+    id: spain12,
+    src: spain12,
+    alt: "spain12",
+  },
+  {
+    id: spain13,
+    src: spain13,
+    alt: "spain13",
+  },
+  {
+    id: spain14,
+    src: spain14,
+    alt: "spain14",
+  },
+  {
+    id: spain15,
+    src: spain15,
+    alt: "spain15",
+  },
+  {
+    id: vat,
+    src: vat,
+    alt: "vat",
+  },
+  {
+    id: italy,
+    src: italy,
+    alt: "italy",
+  },
+  {
+    id: italy1,
+    src: italy1,
+    alt: "italy1",
+  },
+  {
+    id: italy2,
+    src: italy2,
+    alt: "italy2",
+  },
+  {
+    id: italy3,
+    src: italy3,
+    alt: "italy3",
+  },
+  {
+    id: italy4,
+    src: italy4,
+    alt: "italy4",
+  },
+  {
+    id: italy5,
+    src: italy5,
+    alt: "italy5",
+  },
+  {
+    id: italy6,
+    src: italy6,
+    alt: "italy6",
+  },
+  {
+    id: italy7,
+    src: italy7,
+    alt: "italy7",
+  },
+  {
+    id: italy8,
+    src: italy8,
+    alt: "italy8",
+  },
+  {
+    id: menorca,
+    src: menorca,
+    alt: "menorca",
+  },
+  {
+    id: menorca1,
+    src: menorca1,
+    alt: "menorca1",
+  },
+  {
+    id: menorca3,
+    src: menorca3,
+    alt: "menorca3",
+  },
+  {
+    id: menorca4,
+    src: menorca4,
+    alt: "menorca4",
+  },
+  {
+    id: menorca5,
+    src: menorca5,
+    alt: "menorca5",
+  },
+  {
+    id: menorca6,
+    src: menorca6,
+    alt: "menorca6",
+  },
+  {
+    id: sanFran,
+    src: sanFran,
+    alt: "sanFran",
+  },
+  {
+    id: santaBarb,
+    src: santaBarb,
+    alt: "santaBarb",
+  },
+  {
+    id: slo,
+    src: slo,
+    alt: "slo",
+  },
+  {
+    id: slo1,
+    src: slo1,
+    alt: "slo1",
+  },
+  {
+    id: slo2,
+    src: slo2,
+    alt: "slo2",
+  },
+  {
+    id: slo3,
+    src: slo3,
+    alt: "slo3",
+  },
+  {
+    id: slo4,
+    src: slo4,
+    alt: "slo4",
+  },
+  {
+    id: slo5,
+    src: slo5,
+    alt: "slo5",
+  },
+  {
+    id: la,
+    src: la,
+    alt: "la",
+  },
+  {
+    id: la1,
+    src: la1,
+    alt: "la1",
+  },
+  {
+    id: la2,
+    src: la2,
+    alt: "la2",
+  },
+  {
+    id: la3,
+    src: la3,
+    alt: "la3",
+  },
+  {
+    id: lake,
+    src: lake,
+    alt: "lake",
+  },
+  {
+    id: lake2,
+    src: lake2,
+    alt: "lake2",
+  },
+  {
+    id: si,
+    src: si,
+    alt: "si",
+  },
+  {
+    id: stevens,
+    src: stevens,
+    alt: "stevens",
+  },
+  {
+    id: spokane,
+    src: spokane,
+    alt: "spokane",
+  },
+  {
+    id: newport,
+    src: newport,
+    alt: "newport",
+  },
+  {
+    id: newport1,
+    src: newport1,
+    alt: "newport1",
+  },
+  {
+    id: redbuff,
+    src: redbuff,
+    alt: "redbuff",
+  },
+  {
+    id: bellevue,
+    src: bellevue,
+    alt: "bellevue",
+  },
 
-  ];
+];
 
 function Box({ children, ...props }) {
-    return <div {...props}>{children}</div>
-  }
+  return <div {...props}>{children}</div>
+}
 
 function PersonalBlog() {
-    return (
-        <div id="PersonalBlog">
-        <Box class="box" id="disclaimer"> 
-            <h1 style={{color: 'rgb(194, 125, 15)', }}>Personal Blog</h1>
-            <p>Below are some photo highlights of my trips since March of 2023</p> 
-            <p>Places include: Spain, Italy, Menorca, San Francisco, Santa Barbra, SLO, Los Angeles, Lake Alice, Snoqualmie Pass, Spokane, Red Buffs, Newport OR</p>
-        </Box>
-        <br></br>
-        <div>
-            {imagesList.map((image) => (
-                <img src={image.src} alt={image.alt} class="photo" />
-            ))}
-        </div>
+  return (
+    <div id="PersonalBlog">
+      <Box class="box" id="disclaimer">
+        <h1 style={{ color: 'rgb(194, 125, 15)', }}>Personal Blog</h1>
+        <p>Below are some photo highlights of my trips since March of 2023</p>
+        <p>Places include: Spain, Italy, Menorca, San Francisco, Santa Barbra, SLO, Los Angeles, Lake Alice, Snoqualmie Pass, Spokane, Red Buffs, Newport OR, Luisville CO, Seattle</p>
+      </Box>
+      <br></br>
+      <div>
+        {imagesList.map((image) => (
+          <img src={image.src} alt={image.alt} class="photo" />
+        ))}
+      </div>
 
-        <p style={{textAlign:"center", fontSize:25}}>Copyright © Marina Rosenwald 2023</p>
-        <p style={{textAlign:"center", fontSize:25}}>Please contact me for use of any photos email: marinarosenwald@gmail.com</p>
+      <p style={{ textAlign: "center", fontSize: 25 }}>Copyright © Marina Rosenwald 2024</p>
+      <p style={{ textAlign: "center", fontSize: 25 }}>Please contact me for use of any photos email: marinarosenwald@gmail.com</p>
 
     </div>
-    )
+  )
 }
-  
+
 export default PersonalBlog;
